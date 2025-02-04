8ad3ffb0bff0aebbf4dfc0ae1768f4ad6e11b4f7
Elton Soares <eltonls@proton.me>
2025-02-04 19:33:33 -0300

    initial commit

---
+++ chadwm/scripts/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+xrdb merge ~/.Xresources 
+xbacklight -set 10 &
+feh --bg-fill ~/Pictures/wallpapers/jinx.jpg &
+megasync &
+flameshot &
+xset r rate 200 50 &
+picom &
+
+dash ~/.config/chadwm/scripts/bar.sh &
+while type chadwm >/dev/null; do chadwm && continue || break; done
